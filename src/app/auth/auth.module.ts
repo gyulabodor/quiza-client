a32beec1808aaf3da48d6d6ab0ae6b3081f00b8d
gyulabodor95@ <gyulabodor95@gmail.com>
2024-10-22 01:49:59 +0200

    add registration component to auth.module.ts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthFormComponent } from './auth-form/auth-form.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { RegistrationComponent } from './registration/registration.component';
 import {AuthRoutingModule} from "./auth-routing.module";
 import {HttpClientModule} from "@angular/common/http";
 
@@ -11,7 +11,7 @@ import {HttpClientModule} from "@angular/common/http";
   declarations: [
     AuthFormComponent,
     LoginComponent,
-    RegisterComponent
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
