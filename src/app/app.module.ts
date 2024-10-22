85c03d6a29fa920ccf444d52a49b6172957c8038
gyulabodor95@ <gyulabodor95@gmail.com>
2024-10-22 03:16:06 +0200

    add newly generated components and modules to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import { NotExistingPageComponent } from './pages/not-existing-page/not-existing-page.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    NotExistingPageComponent,
+    WelcomePageComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -20,6 +25,7 @@ import {MatButtonModule} from "@angular/material/button";
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
