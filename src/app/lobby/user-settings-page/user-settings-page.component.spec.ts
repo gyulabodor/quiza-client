819d041388d5ac24d12fc59c51ea3269507e7c73
gyulabodor95@ <gyulabodor95@gmail.com>
2024-10-28 21:38:59 +0100

    reformat code with prettier

--- src/app/lobby/user-settings-page/user-settings-page.component.spec.ts
+++ src/app/lobby/user-settings-page/user-settings-page.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserSettingsPageComponent } from './user-settings-page.component';
+import {UserSettingsPageComponent} from './user-settings-page.component';
 
 describe('UserSettingsPageComponent', () => {
   let component: UserSettingsPageComponent;
