dd1ec4ba8599898f5a1a0740375a1f8667ad599e
gyulabodor95@ <gyulabodor95@gmail.com>
2024-10-28 20:57:16 +0100

    prepare page component for user-settings

--- src/app/lobby/user-settings-page/user-settings-page.component.ts
+++ src/app/lobby/user-settings-page/user-settings-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'user-settings-page',
   templateUrl: './user-settings-page.component.html',
   styleUrls: ['./user-settings-page.component.scss']
 })
-export class UserSettingsPageComponent {
+export class UserSettingsPageComponent extends Component implements OnInit {
+  public constructor() {
+    super();
+  }
+  ngOnInit(): void {
+  }
 
 }
