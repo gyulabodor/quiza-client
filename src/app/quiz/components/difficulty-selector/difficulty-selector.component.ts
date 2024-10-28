825dac7fac6276735be4de517aba8f89b932cd30
gyulabodor95@ <gyulabodor95@gmail.com>
2024-10-28 20:55:05 +0100

    create difficulty-selector component where you can select diff level you want to play

---
+++ src/app/quiz/components/difficulty-selector/difficulty-selector.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {DifficultyEnum} from "../../../../globals/enums/difficulty.enum";
+import {BehaviorSubject} from "rxjs";
+
+@Component({
+  selector: 'app-difficulty-selector',
+  templateUrl: './difficulty-selector.component.html',
+  styleUrls: ['./difficulty-selector.component.scss']
+})
+export class DifficultySelectorComponent implements OnInit {
+
+  public selectedDifficulty$?: BehaviorSubject<DifficultyEnum>;
+
+  ngOnInit(): void {
+    this.selectedDifficulty$ = new BehaviorSubject<DifficultyEnum>(DifficultyEnum.EASY);
+  }
+
+  public registerOnChanges(): void {
+
+  }
+}
