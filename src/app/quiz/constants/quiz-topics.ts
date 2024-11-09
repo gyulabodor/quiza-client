928e86d680490cfc2eafc8e10521368fa664e215
gyulabodor95@ <gyulabodor95@gmail.com>
2024-11-09 21:20:41 +0100

    predefine quiz-topics array

---
+++ src/app/quiz/constants/quiz-topics.ts
@@ -0,0 +1,10 @@
+import {QuizTopic} from "../models/quiz-topic.model";
+import {Topic} from "../../shared/globals/enums/topic.enum";
+
+export const QUIZ_TOPIC_LIST: QuizTopic[] = [
+
+{name: Topic.HISTORY, alreadyPlayed: false} as QuizTopic,
+
+
+];
+export const QUIZ_TOPIC_LIST_LENGTH: number = QUIZ_TOPIC_LIST.length
