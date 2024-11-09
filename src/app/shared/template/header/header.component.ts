2a9cb55ff3c90e9a4d9372469fe03d0a16c1c6be
gyulabodor95@ <gyulabodor95@gmail.com>
2024-11-09 21:09:40 +0100

    create header layout and start implementing logic for navigation menu

--- src/app/shared/template/header/header.component.ts
+++ src/app/shared/template/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem, pageList} from "../../globals/pages/page-list";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'mobile-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  //todo.: upload nav menu items with page list
+  public pages$= new BehaviorSubject<MenuItem[]>([]);
+
+  ngOnInit(): void {
+    this.pages$.next(pageList);
+  }
 
 }
