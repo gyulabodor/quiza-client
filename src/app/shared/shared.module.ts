13594c06d7362ec3ffac2ea25e77a02088cd8d24
gyulabodor95@ <gyulabodor95@gmail.com>
2024-11-09 21:13:53 +0100

    add CUSTOM_ELEMENTS_SCHEMA to handle schemas and replace header.component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UserSettingsPageComponent} from "./globals/pages/user-settings-page/user-settings-page.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import {HeaderComponent} from "./template/header/header.component";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {RouterLink} from "@angular/router";
 
 @NgModule({
   declarations: [
     UserSettingsPageComponent,
+    HeaderComponent
   ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        RouterLink,
+    ],
   exports: [
     UserSettingsPageComponent,
-  ]
+    HeaderComponent,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule { }
