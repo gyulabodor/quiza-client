3889009a352eee595e537481669dda01158807b1
gyulabodor95@ <gyulabodor95@gmail.com>
2024-11-09 21:10:22 +0100

    create page-list.ts for testing

---
+++ src/app/shared/globals/pages/page-list.ts
@@ -0,0 +1,11 @@
+export const pageList: MenuItem[] = [
+  {
+    name: "difficulty-selector",
+    url:'/quiz/select-difficulty'
+  },
+]
+
+export interface MenuItem {
+  name: string,
+  url: string,
+}
