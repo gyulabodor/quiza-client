0c7c508e443fd049455b1941581ec9c418cc87ef
gyulabodor95@ <gyulabodor95@gmail.com>
2024-11-22 09:58:06 +0100

    define quiz topics

--- src/app/quiz/constants/quiz-topics.ts
+++ src/app/quiz/constants/quiz-topics.ts
@@ -3,8 +3,14 @@ import {Topic} from "../../shared/globals/enums/topic.enum";
 
 export const QUIZ_TOPIC_LIST: QuizTopic[] = [
 
-{name: Topic.HISTORY, alreadyPlayed: false} as QuizTopic,
-
-
+{id:1,name: Topic.CHEMISTRY, played: false} as QuizTopic,
+{id:2,name: Topic.HISTORY, played: false} as QuizTopic,
+{id:3,name: Topic.ANIMALS, played: false} as QuizTopic,
+{id:4,name: Topic.BIOLOGY, played: false} as QuizTopic,
+{id:5,name: Topic.INFORMATION_TECHNOLOGY, played: false} as QuizTopic,
+{id:6,name: Topic.LITERATURE, played: false} as QuizTopic,
+{id:7,name: Topic.PHYSICS, played: false} as QuizTopic,
+{id:8,name: Topic.PLANTS, played: false} as QuizTopic,
+{id:9,name: Topic.GEOGRAPHY, played: false} as QuizTopic,
+{id:10,name: Topic.RANDOM, played: false} as QuizTopic,
 ];
-export const QUIZ_TOPIC_LIST_LENGTH: number = QUIZ_TOPIC_LIST.length
